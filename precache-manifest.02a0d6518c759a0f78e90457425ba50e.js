9fa97d58e49bfc77fdaa88e7d26ca121618ad882
jlonergan99 <74012512+jlonergan99@users.noreply.github.com>
2020-11-09 08:14:57 -0500

    Add files via upload

---
+++ precache-manifest.02a0d6518c759a0f78e90457425ba50e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f747d53cfeb88a75fc5e28ffc5a0e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2f34cbf488bdfc97dc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0a438e6419d12612c385",
+    "url": "/static/css/main.fb8c703e.chunk.css"
+  },
+  {
+    "revision": "0a2f34cbf488bdfc97dc",
+    "url": "/static/js/2.9834437a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9834437a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a438e6419d12612c385",
+    "url": "/static/js/main.01213812.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "a068050cacdfa6e9326034044eff2c57",
+    "url": "/static/media/LogoFull.a068050c.png"
+  },
+  {
+    "revision": "024dfc4439cd0c1c2c087a38bab64cc7",
+    "url": "/static/media/Plaque.024dfc44.png"
+  }
+]);
